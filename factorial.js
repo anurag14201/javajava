107220d57123631ea77dc420bf815d512d3778f7
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-05 12:25:53 +0600

    reverse

--- factorial.js
+++ factorial.js
@@ -1,12 +1,24 @@
+// function getFactorial(number) {
+//     let factorial = 1;
+//     let i = 1;
+//     while (i <= number) {
+//         factorial = factorial * i;
+//         i++;
+//     }
+//     return factorial;
+// }
+
+// const myFactorial = getFactorial(12);
+// console.log(myFactorial);
+
 function getFactorial(number) {
     let factorial = 1;
-    let i = 1;
-    while (i <= number) {
+    let i = number;
+    while (i >= 1) {
         factorial = factorial * i;
-        i++;
+        i--;
     }
     return factorial;
 }
-
-const myFactorial = getFactorial(12);
+const myFactorial = getFactorial(7);
 console.log(myFactorial);
