84d10b4ab150c3e052624a85df0b0117d90d78ce
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-06 16:04:22 +0600

    math

---
+++ math.js
@@ -0,0 +1,21 @@
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+// console.log(output);
+
+
+// const myNumber = 6.6398;
+// floor/ceil/round/random
+// const output = Math.floor(myNumber);
+// console.log(output);
+
+
+// const myNumber = 68.009;
+// const output = Math.random() * 6;
+// const rounded = Math.round(output);
+// console.log(rounded);
+
+for (let i = 0; i <= 30; i++) {
+    const output = Math.random() * 6;
+    const rounded = Math.round(output);
+    console.log(rounded);
+}
