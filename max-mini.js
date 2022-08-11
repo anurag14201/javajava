4a1468817826f33f1c8a174128bf7f313f1dc71e
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-11 10:42:12 +0600

    multiply

---
+++ max-mini.js
@@ -0,0 +1,10 @@
+// var number = 330;
+// var number1 = 440;
+// var number2 = 550;
+// for()
+
+const numbers = [44, 45, 77, 54, 86, 1];
+for (let i = 0; i < numbers.length; i++) {
+    // const element = numbers[i];
+    console.log();
+}
---
+++ multiply.js
@@ -0,0 +1,17 @@
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+const firstTottal = add(77, 3);
+// console.log(firstTottal);
+
+const secoundTotal = add(55, 5);
+// console.log(secoundTotal);
+
+function multiply(num1, num2) {
+    const result = num1 * num2;
+    return result;
+}
+const firstResult = multiply(5, 5);
+console.log(firstResult);
